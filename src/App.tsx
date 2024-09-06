6e91a94e96437c5293080916fd84e4c9e1b74bba
Akash patel <akashpatel10000@gmail.com>
2024-09-06 15:18:25 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ModeToggle } from "./components/mode-toggle";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <ModeToggle/>
+    </div>
+  )
+}
