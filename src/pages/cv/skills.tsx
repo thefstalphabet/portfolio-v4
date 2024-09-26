928e48c00d2548f210f66fb5e189bed89bc9e574
Akash patel <akashpatel10000@gmail.com>
2024-09-26 18:40:29 +0530

    update: layout

--- src/pages/cv/skills.tsx
+++ src/pages/cv/skills.tsx
@@ -3,18 +3,24 @@ export default function Skills() {
     <div>
       <h4 className="font-bold">Skills</h4>
       <hr className="mt-2 mb-0.5 border-neutral-600" />{" "}
-      <div className="text-sm flex gap-10">
+      <div className="text-sm flex gap-5">
         <div>
-          <h5 className="font-semibold">Programming Languages:</h5>
-          <h5 className="font-semibold">Libraries/Frameworks:</h5>
-          <h5 className="font-semibold">Tools/Platforms:</h5>
-          <h5 className="font-semibold">Databases:</h5>
-        </div>
-        <div>
-          <p>JavaScript, C++, TypeScript</p>
-          <p>React, Tailwind, Nest, Redux, Ant Design, SchadCn</p>
-          <p>Git, GitHub, Jira, Photoshop, Canva</p>
-          <p>MongoDB, PostgreSQL, Supabase</p>
+          <div className="flex gap-2 items-center">
+            <h5 className="font-semibold">Programming Languages:</h5>{" "}
+            <span>JavaScript, C++, TypeScript</span>
+          </div>
+          <div className="flex gap-2 items-center">
+            <h5 className="font-semibold">Libraries / Frameworks:</h5>{" "}
+            <span>React, Tailwind, Nest, Redux, Ant Design, SchadCn</span>
+          </div>
+          <div className="flex gap-2 items-center">
+            <h5 className="font-semibold">Databases:</h5>{" "}
+            <span>MongoDB, PostgreSQL, Supabase</span>
+          </div>
+          <div className="flex gap-2 items-center">
+            <h5 className="font-semibold">Tools / Platforms:</h5>{" "}
+            <span>Git, GitHub, Jira, Photoshop, Canva</span>
+          </div>
         </div>
       </div>
     </div>
