3fb39562df3ef4df7df3fa7fb2e3b6d6497f0388
Akash patel <akashpatel10000@gmail.com>
2024-09-26 18:18:04 +0530

    update: added one more project in cv

--- src/pages/cv/projects.tsx
+++ src/pages/cv/projects.tsx
@@ -7,6 +7,31 @@ export default function Projects() {
       <div className="text-sm">
         <div>
           <div className="flex gap-1">
+            <h5 className="font-semibold">Get Hired | </h5>
+            <Link
+              to="https://get-hired-mu.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Url
+            </Link>
+          </div>
+          <ul className="px-2">
+            <div className="flex gap-2">
+              <p>•</p>
+              <li>Developing a SaaS Product for Recruiting Professionals.</li>
+            </div>
+            <div className="flex gap-2">
+              <p>•</p>
+              <li>
+                Used React(vite) and ScadCn for developing user interfaces, and
+                Supabase for backend.
+              </li>
+            </div>
+          </ul>
+        </div>
+        <div>
+          <div className="flex gap-1">
             <h5 className="font-semibold">Lunch Box | </h5>
             <Link
               to="https://tiffin-box.netlify.app/"
@@ -20,7 +45,7 @@ export default function Projects() {
             <div className="flex gap-2">
               <p>•</p>
               <li>
-                Creating a SaaS product that offers working professionals and
+                Developing a SaaS product that offers working professionals and
                 students an easy way to manage their daily meals.
               </li>
             </div>
